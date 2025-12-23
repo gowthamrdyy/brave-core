c51ee75d41adce4fd6300b04d4049429ce827df4
Szilard Szaloki <szilardszaloki@gmail.com>
2025-12-22 17:46:57 -0700

    Brave Account: email address (`/v2/auth/validate`)

--- components/brave_account/brave_account_service_constants.h
+++ components/brave_account/brave_account_service_constants.h
@@ -14,6 +14,10 @@ inline constexpr base::TimeDelta kVerifyResultPollInterval = base::Seconds(5);
 inline constexpr base::TimeDelta kVerifyResultWatchdogInterval =
     3 * kVerifyResultPollInterval;
 
+inline constexpr base::TimeDelta kAuthValidatePollInterval = base::Minutes(2);
+inline constexpr base::TimeDelta kAuthValidateWatchdogInterval =
+    base::Minutes(2);
+
 }  // namespace brave_account
 
 #endif  // BRAVE_COMPONENTS_BRAVE_ACCOUNT_BRAVE_ACCOUNT_SERVICE_CONSTANTS_H_
